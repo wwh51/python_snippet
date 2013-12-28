dccb5e1240c5e7e8c7db1a01e90762f18da7f515
David Wang <weihuaw@gmail.com>
2013-12-28 19:40:54 +1100

    add slow and fast methods and unit tests

--- getnumfactors.py
+++ getnumfactors.py
@@ -1,4 +1,39 @@
-def getnumfactors(x):
+import unittest
+import time
+
+def get_all_factors(x):
+    factors = []
+    factors.append(1)
+    factors.append(x)
+    i = 2
+    mfactor = x
+    while True:
+        if x % i == 0:
+            factors.append(i)
+            mfactor = x // i
+            if(mfactor > i):
+                factors.append(mfactor)
+        i += 1
+        if(i >= mfactor):
+            break
+    return factors
+
+def getnumfactors_slow(x):
+    l=2
+    i = 2
+    mfactor = x
+    while True:
+        if x % i == 0:
+            l += 1
+            mfactor = x // i
+            if(mfactor > i):
+                l += 1
+        i += 1
+        if(i >= mfactor):
+            break
+    return l
+
+def getnumfactors_fast(x):
     if x == 1 or x == 2:
         return x
     k = 0
@@ -20,3 +55,40 @@ def getnumfactors(x):
     if x > 1:
         count = count << 1
     return count
+
+
+class TestGetnumfactors(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_speed(self):
+        t1=time.time()
+        nstart = 7
+        ntriangle = 28
+        while True:
+            if(getnumfactors_fast(ntriangle) > 200):
+                break
+            nstart += 1
+            ntriangle += nstart
+        res_1 = ntriangle
+        t2=time.time()
+        nstart = 7
+        ntriangle = 28
+        while True:
+            if(getnumfactors_slow(ntriangle) > 200):
+                break
+            nstart += 1
+            ntriangle += nstart
+        t3 = time.time()
+        self.assertEqual(ntriangle, res_1)
+        self.assertLessEqual(t2-t1, t3-t2)
+        # print(ntriangle)
+        # print(t2-t1)
+        # print(t3-t2)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
