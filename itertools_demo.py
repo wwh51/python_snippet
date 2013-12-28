d13c42795a03876e7102a8f9e7b31bc212e1cbec
David Wang <weihuaw@gmail.com>
2013-12-28 23:37:07 +1100

    Update itertools_demo.py

--- itertools_demo.py
+++ itertools_demo.py
@@ -47,6 +47,63 @@ class Test_itertoos(unittest.TestCase):
             ['A', 'C', 'E', 'F']
         )
 
+        self.assertEqual(
+            list( itertools.dropwhile(lambda x: x<5, [1,4,6,4,1])  ),
+            [6,4,1]
+        )
+
+        self.assertEqual(
+            list( itertools.takewhile(lambda x: x<5, [1,4,6,4,1])  ),
+            [1,4]
+        )
+
+        self.assertEqual(
+            list(filter(lambda x:x%2, range(10) ) ),
+            [1, 3, 5, 7, 9]
+        )
+
+        self.assertEqual(
+            list(itertools.filterfalse(lambda x:x%2, range(10) ) ),
+            [0, 2, 4, 6, 8]
+        )
+
+        self.assertEqual(
+            list(itertools.islice(range(100), 10,70,10)), #seq, [start,] stop [, step]
+            [10, 20, 30, 40, 50, 60]
+        )
+
+        self.assertEqual(
+            list(itertools.zip_longest('ABCD', 'xy', fillvalue='-')),
+            [('A', 'x'), ('B', 'y'), ('C', '-'), ('D', '-')]
+            )
+
+        self.assertEqual(
+            list(itertools.starmap(pow, [(2,5), (3,2), (10,3)]) ),
+            [32, 9, 1000]
+        )
+
+        groups = []
+        uniquekeys = []
+        keyfunc = lambda x: x%3
+        data = list(range(20))
+        data = sorted(data, key=keyfunc)
+        for k, g in itertools.groupby(data, keyfunc):
+           groups.append(list(g))
+           uniquekeys.append(k)
+        self.assertEqual(uniquekeys, [0, 1, 2])
+        self.assertEqual(groups,
+            [[0, 3, 6, 9, 12, 15, 18], [1, 4, 7, 10, 13, 16, 19], [2, 5, 8, 11, 14, 17]])
+
+
+        i= range(10)
+        t=itertools.tee(i,3)
+        a,b,c = next(t[0]), next(t[0]), next(t[0])
+        self.assertEqual( (a,b,c), (0,1,2) )
+        a,b,c = next(t[2]), next(t[2]), next(t[2])
+        self.assertEqual( (a,b,c), (0,1,2) )
+        a,b,c = next(t[0]), next(t[0]), next(t[0])
+        self.assertEqual( (a,b,c), (3,4,5) )
+
     def test_Combinatoric_generators(self):
         #product
         self.assertEqual(
@@ -77,16 +134,6 @@ class Test_itertoos(unittest.TestCase):
             [''.join(s) for s in itertools.combinations_with_replacement('ABCD', 2)],
             ['AA', 'AB', 'AC', 'AD', 'BB', 'BC', 'BD', 'CC', 'CD', 'DD']
         )
-
-        i= range(10)
-        t=itertools.tee(i,3)
-        a,b,c = next(t[0]), next(t[0]), next(t[0])
-        self.assertEqual( (a,b,c), (0,1,2) )
-        a,b,c = next(t[2]), next(t[2]), next(t[2])
-        self.assertEqual( (a,b,c), (0,1,2) )
-        a,b,c = next(t[0]), next(t[0]), next(t[0])
-        self.assertEqual( (a,b,c), (3,4,5) )
-
 
 if __name__== '__main__':
     unittest.main()
