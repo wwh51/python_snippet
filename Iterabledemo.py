59d690e154ab52b725372cfe4dbce7a5c86cf67c
David Wang <weihuaw@gmail.com>
2013-12-28 19:19:33 +1100

    Create Iterabledemo.py
    show how to use itertools

---
+++ Iterabledemo.py
@@ -0,0 +1,13 @@
+from collections import Iterable
+
+# 1 yield from ,flatten, flatten
+def flatten(items, ignore_types=(str, bytes)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield from flatten(x)
+        else:
+            yield x
+items = [1, 2, [3, 4, [5, 6], 7], 8]
+# Produces 1 2 3 4 5 6 7 8
+for x in flatten(items):
+    print(x)
