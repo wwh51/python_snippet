419e10762fac81bed121b13401c0e1b0a569451c
David Wang <weihuaw@gmail.com>
2013-12-31 21:14:54 +1100

    Update Euler14.py
    l -> seq_len

--- Euler14.py
+++ Euler14.py
@@ -1,31 +1,30 @@
-
 import time
 def get_seq_len(x, dict_Num_SeqLen):
-    l = 1
+    seq_len = 1
     dict_Tmp={}
     while(x != 1):
         if x in dict_Num_SeqLen:
-            l += dict_Num_SeqLen[x]
+            seq_len += dict_Num_SeqLen[x]
             break
         else:
-            dict_Tmp[x] = l
+            dict_Tmp[x] = seq_len
         if x%2 == 0:
             x = x//2
         else:
             x = x*3+1
-        l += 1
+        seq_len += 1
     for k,v in dict_Tmp.items():
-        dict_Num_SeqLen[k] = l - v
-    return l
+        dict_Num_SeqLen[k] = seq_len - v
+    return seq_len
 
 longest_len = 0
 longest_num = 0
 dict_Num_SeqLen = {}
 time_begin = time.time()
 for num in range(3,1000000):
-    l = get_seq_len(num, dict_Num_SeqLen)
-    if l > longest_len:
-        longest_len = l
+    seq_len = get_seq_len(num, dict_Num_SeqLen)
+    if seq_len > longest_len:
+        longest_len = seq_len
         longest_num = num
 
 print('The starting number %d produce the seq length %d' %(longest_num, longest_len) )
